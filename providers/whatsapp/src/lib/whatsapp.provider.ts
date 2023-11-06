74419c357f79c58c773934fe252f096cf80b79a7
RumanOnMove <oruman@moveon.com.bd>
2023-11-06 18:10:02 +0600

    fix: whatsapp provider exception fixed

--- providers/whatsapp/src/lib/whatsapp.provider.ts
+++ providers/whatsapp/src/lib/whatsapp.provider.ts
@@ -4,21 +4,36 @@ import {
   IChatOptions,
   IChatProvider,
 } from '@novu/stateless';
+import axios from 'axios';
 
 export class WhatsappChatProvider implements IChatProvider {
   id = 'whatsapp';
   channelType = ChannelTypeEnum.CHAT as ChannelTypeEnum.CHAT;
+  private axiosInstance = axios.create();
 
   constructor(private config: { apiKey?: string }) {}
 
   async sendMessage(
     options: IChatOptions
   ): Promise<ISendMessageSuccessResponse> {
-    console.log('Hello world');
+    const body = {
+      alert_uid: this.config.apiKey,
+      message: options.content,
+    };
+
+    // eslint-disable-next-line no-console
+    console.log('Hello world', body);
+    /*
+     * response is just string "Ok."
+     *  const { headers } = await this.axiosInstance.post(
+     *    'https://localhost:8080/api/test',
+     *    body
+     *  );
+     */
 
     return {
-      id: 'id_returned_by_provider',
-      date: 'current_time',
+      id: '12345',
+      date: new Date().toISOString(),
     };
   }
 }
